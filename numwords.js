fe60d785065797db8078d845333c24de56acda5f
Mauskin <cyril@Cyrils-MacBook-Air.local>
2015-01-11 23:12:50 +0300

    Adding script

---
+++ numwords.js
@@ -0,0 +1,73 @@
+// Run in Terminal
+// $ /System/Library/Frameworks/JavaScriptCore.framework/Versions/Current/Resources/jsc Documents/numwords.js
+
+var max = 999;
+var mLen = (max+"").length;
+
+for (var i = 0; i <= max; i++) debug( "      ".substr(1, mLen - (i+"").length) + i + ": " + numWords(i) + "." );
+
+function numWords( num ){
+	
+	// Return zero right away 
+	if (num === 0) return "ноль";
+	
+	var _this = this;
+	
+	// Clear non numbers
+	this.numString = (num+"").match(/\d/ig).join("");
+	
+	if ( !(this.numString.length > 0) ) return NaN;
+
+	var len = this.numString.length;
+
+	var groupCount = Math.floor( (len - 1) / 3 );
+
+	var numArray = [];
+			
+	for (var i = 0 ; i <= groupCount ; i++ ){
+	
+		var groupLength = 3;
+	
+		if ( ( len - ( 3 * i ) ) < 3 ) { groupLength = len - ( i * 3 ); }
+	
+		var groupString = numString.substr( - ( i * 3 + groupLength), groupLength );
+	
+		numArray[i] = ("00" + groupString).substr(-3);
+
+	}
+	
+	var units       = [, "один",       "два",        "три",        "четыре",       "пять",       "шесть",       "семь",       "восемь",       "девять",
+			   "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" ];
+	var tens        = [,,		        "двадцать",   "тридцать",   "сорок",        "пятьдесят",  "шестьдесят",  "семьдесят",  "восемьдесят",  "девяносто"    ];
+	var hundreds    = [, "сто",        "двести",     "триста",     "четыреста",    "пятьсот",    "шестьсот",    "семьсот",    "восемьсот",    "девятсот"     ];
+
+ 	var thousands = [ [ "тысяча",   "тысячи",    "тысяч"      ], 
+ 				 	  [  "миллион",  "миллиона",  "миллионов"  ], 
+ 					  [ "миллиард", "миллиарда", "миллиардов" ],
+ 					  [ "триллион", "триллиона", "триллионов" ] ];
+
+	
+	function processGroupOfThree( group, groupNo ){
+		
+		var groupWords = [];
+		
+		groupWords.push( hundreds[ Number(group.substr(-3,1)) ] );
+		
+		groupWords.push( tens[ Number(group.substr(-2,1)) ] );
+		
+		groupWords.push( units[ Number(group.substr(-2)) ] );
+		
+		if (Number(group.substr(-2))>20) groupWords.push( units[ Number(group.substr(-1)) ] );
+		return groupWords;
+		
+	}
+	
+	var words = [];
+	
+	for (var i = 0 ; i <= groupCount ; i++){
+		
+		words.unshift(processGroupOfThree( numArray[i], i).join(" "));
+	}
+	
+	return words.join(" ").replace(/\s+/g, " ").trim();
+} 
