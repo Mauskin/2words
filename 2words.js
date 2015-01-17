a2d2002c39234a52b1ef1e058caa70390fa69d4d
Mauskin <cyril@Cyrils-MacBook-Air.local>
2015-01-17 22:15:53 +0300

    This is a huge one
    - Changed the way numbers were divided by groups
    - Added declension of units of thousands

--- 2words.js
+++ 2words.js
@@ -1,73 +1,77 @@
-// Debug start
-var max = 999;
-var mLen = (max+"").length;
-for (var i = 0; i <= max; i++) debug( "      ".substr(1, mLen - (i+"").length) + i + ": " + toWords(i) + "." );
-// Debug end
-
 function toWords(num) {
 	
 	// Return zero right away 
 	if (num === 0) return "ноль";
 	
-	var _this = this;
-	
-	// Clear non numbers
+	// Clear non numbers and check if something is left
 	this.numString = (num+"").match(/\d/ig).join("");
+	if (!(this.numString.length > 0)) return NaN;
 	
-	if ( !(this.numString.length > 0) ) return NaN;
+	// Stick the number to an array
+	this.len = this.numString.length;
+	this.groupCount = Math.floor((len - 1) / 3);
+	this.numString = "00".substring(0, Math.abs(this.len-(this.groupCount+1)*3)) + this.numString;
+	this.numArray = this.numString.match(/\d{3}/g);
+	
+	// Process all groups
+	for (var i = 0; i <= this.groupCount; i++){
+		var groupNum = this.numArray[i];
+		this.numArray[i] = [];
+		this.numArray[i][0] = Number(groupNum.match(/^\d/));
+		if (groupNum.match(/\d{2}$/)[0].match(/^1[1-9]$/) !== null) {
+			this.numArray[i][1] = 0;
+			this.numArray[i][2] = Number(groupNum.match(/\d{2}$/));
+		} else {
+			this.numArray[i][1] = Number(groupNum.match(/\d/g)[1]);
+			this.numArray[i][2] = Number(groupNum.match(/\d$/));
+		}
+	}
+	this.numArray = this.numArray.reverse()
 
-	var len = this.numString.length;
+	// Words
+	var hundreds = [, "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот" ];
+	
+	var tens = [, "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто" ];
+	
+	var units = [, "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", ,
+				   "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" ];
 
-	var groupCount = Math.floor( (len - 1) / 3 );
-
-	var numArray = [];
-			
-	for (var i = 0 ; i <= groupCount ; i++ ){
+ 	var thousands = [, [ "тысяча", "тысячи", "тысяч" ], 
+ 				 	   [ "миллион", "миллиона", "миллионов" ], 
+ 					   [ "миллиард", "миллиарда", "миллиардов" ],
+ 					   [ "триллион", "триллиона", "триллионов" ] ];
 	
-		var groupLength = 3;
-	
-		if ( ( len - ( 3 * i ) ) < 3 ) { groupLength = len - ( i * 3 ); }
-	
-		var groupString = numString.substr( - ( i * 3 + groupLength), groupLength );
-	
-		numArray[i] = ("00" + groupString).substr(-3);
-
+	// Converting numbers to words
+	function processGroupOfThree(group, groupNo){
+		var groupWords = [ "", "", "", "" ];
+		var declNum = Number(group[2]);
+		var decl = 0;
+		switch (true) {
+			case declNum === 0:
+				decl = 2; break;
+			case Math.floor(1 / declNum) > 0:
+				decl = 0; break;
+			case Math.floor(4 / declNum) > 0:
+				decl = 1; break;
+			default:
+				decl = 2;
+			}
+		groupWords[0] = hundreds[ group[0] ];
+		groupWords[1] = tens[ group[1] ];
+		groupWords[2] = units[ group[2] ];
+		if (groupNo > 0) {
+			groupWords[3] = thousands[groupNo][decl];
+			if (groupNo == 1 && group[2] < 3) groupWords[2] = [,"одна", "две"][group[2]];
+		}
+		return groupWords.join(" ");
 	}
 	
-	var units       = [, "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять",
-						 "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" ];
-
-	var tens        = [,, "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто" ];
-
-	var hundreds    = [, "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот" ];
-
- 	var thousands = [ [ "тысяча",   "тысячи",    "тысяч"      ], 
- 				 	  [  "миллион",  "миллиона",  "миллионов"  ], 
- 					  [ "миллиард", "миллиарда", "миллиардов" ],
- 					  [ "триллион", "триллиона", "триллионов" ] ];
-
-	
-	function processGroupOfThree( group, groupNo ){
-		
-		var groupWords = [];
-		
-		groupWords.push( hundreds[ Number(group.substr(-3,1)) ] );
-		
-		groupWords.push( tens[ Number(group.substr(-2,1)) ] );
-		
-		groupWords.push( units[ Number(group.substr(-2)) ] );
-		
-		if (Number(group.substr(-2))>20) groupWords.push( units[ Number(group.substr(-1)) ] );
-		return groupWords;
-		
+// 	Join groups
+	this.words = [];
+	for (var i = 0; i <= this.groupCount; i++){
+		words.unshift( processGroupOfThree(this.numArray[i], i));
 	}
 	
-	var words = [];
-	
-	for (var i = 0 ; i <= groupCount ; i++){
-		
-		words.unshift(processGroupOfThree( numArray[i], i).join(" "));
-	}
-	
+// 	Clear spaces and return final string
 	return words.join(" ").replace(/\s+/g, " ").trim();
-} 
+}
