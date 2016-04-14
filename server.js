7e0b7f9be457bd4e4c3a0f7f0cdca62d4d8f8338
Cyril Myshkin <mauskin@users.noreply.github.com>
2016-04-14 21:15:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,45 +2,46 @@ var http = require('http');
 var toWords = require('./2words');
 var React = require('react');
 
+var NumberConverter = React.createClass({
+  getInitialState: function() {
+    var max = 99999999;
+    var defaultValue = Math.floor(Math.random() * (max - 0 + 1));
+    return {value: defaultValue};
+  },
+  handleChange: function() {
+    this.setState({value: this.refs.input.value});
+  },
+  render: function() {
+    var words = toWords(this.state.value);
+    var output = (words === null)
+      ? 'Что-то менее космическое, пожалуйста.'
+      : words[0].toUpperCase() + words.slice(1) + '.';
+    return (
+      <div className="NumberConverter">
+        <h2>Введите число</h2>
+        <input
+          onChange={this.handleChange}
+          ref="input"
+          defaultValue={this.state.value} />
+        <h3>Результат</h3>
+        <div className="content">{output}</div>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<NumberConverter />,
+  document.getElementById('container')
+);
+
 http.createServer(function(req, res) {
 	if (req.url == '/') {
 		res.setHeader('Content-Type', 'text/html');
 		res.end(
-			'<div id="NumberConverter">' + '</div>' +
-			'<script src=//fb.me/react-0.12.2.min.js></script>' + //TODO load React locally
-			'<script>' +
-				'var toWords = ' + toWords + ';' +
-				'var NumberConverter = React.createClass({displayName: "NumberConverter",'+
-					'getInitialState: function() {'+
-						'return {value: "0"};'+
-					'},'+
-					'handleChange: function() {'+
-						'this.setState({value: this.refs.input.getDOMNode().value});'+
-					'},'+
-					'render: function() {'+
-						'return ('+
-							'React.createElement("div", {Id: "NumberConverter"}, '+
-								'React.createElement("h3", null, "Input"), '+
-								'React.createElement("input", {'+
-									'type: "number",' +
-									'inputmode: "numeric",' +
-									'onChange: this.handleChange, '+
-									'ref: "input", '+
-									'defaultValue: this.state.value}), '+
-								'React.createElement("h3", null, "Output"), '+
-								'React.createElement("div", {'+
-									'className: "content", '+
-									'dangerouslySetInnerHTML: {'+
-										'__html: toWords(this.state.value)'+
-									'}}'+
-								')'+
-							')'+
-						');'+
-					'}'+
-				'});' +
-        'React.render(React.createElement(NumberConverter, null), document.getElementById("NumberConverter"))' +
-      '</script>'
-    )
+			// TODO: load React locally
+			'<script src=//fb.me/react-0.12.2.min.js></script>' +
+			'<div id="NumberConverter">' + '</div>'
+    		)
 	} else {
 		res.statusCode = 404;
 		res.end();
